c5f229694f9c80c21c280a459dc169876abc2aab
Riyaz Qureshi <42235756+r4riyaz@users.noreply.github.com>
2026-02-19 17:31:42 +0530

    Update outputs.tf

--- eks-production-platform/terraform/eks/outputs.tf
+++ eks-production-platform/terraform/eks/outputs.tf
@@ -1,3 +1,11 @@
-output "cluster_name" {
-  value = module.eks.cluster_name
+output "oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
+}
+
+output "oidc_provider" {
+  value = module.eks.oidc_provider
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
 }
