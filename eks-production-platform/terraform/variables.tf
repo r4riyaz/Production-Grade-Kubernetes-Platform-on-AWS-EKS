a4ddbf7cd28cfa3ccbd630837ced16edd17bfdb2
Riyaz Qureshi <42235756+r4riyaz@users.noreply.github.com>
2026-02-19 17:30:44 +0530

    Create variables.tf

---
+++ eks-production-platform/terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "cluster_name" {
+  description = "EKS Cluster Name"
+  type        = string
+  default     = "eks-prod"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "az_count" {
+  description = "Number of AZs"
+  type        = number
+  default     = 1
+}
