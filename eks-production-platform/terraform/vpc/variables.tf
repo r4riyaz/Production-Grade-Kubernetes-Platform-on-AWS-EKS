b4cd832042aa6dd823f6ff6540987f31da8186b7
Riyaz Qureshi <42235756+r4riyaz@users.noreply.github.com>
2026-02-19 16:52:47 +0530

    Update variables.tf

--- eks-production-platform/terraform/vpc/variables.tf
+++ eks-production-platform/terraform/vpc/variables.tf
@@ -1,3 +1,29 @@
 variable "region" {
-  default = "ap-south-1"
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name (used for subnet tagging)"
+  type        = string
+  default     = "eks-prod"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "az_count" {
+  description = "Number of availability zones"
+  type        = number
+  default     = 1
 }
