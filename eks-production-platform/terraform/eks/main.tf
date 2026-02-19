fa1d2720f2b8eeeaf0b75e09db0417cb7db61e13
Riyaz Qureshi <42235756+r4riyaz@users.noreply.github.com>
2026-02-19 18:06:58 +0530

    Update main.tf

--- eks-production-platform/terraform/eks/main.tf
+++ eks-production-platform/terraform/eks/main.tf
@@ -1,8 +1,9 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  cluster_version = var.cluster_version
 
   vpc_id     = var.vpc_id
   subnet_ids = var.subnet_ids
